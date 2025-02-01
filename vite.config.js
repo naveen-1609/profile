c1e683726829fd81778fd55f9aa0c291096c9b69
Naveen Venkat <naveenvenkat58@gmail.com>
2025-02-01 18:23:19 -0500

    third deploy

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,8 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/profile/', // ✅ Ensure this matches your GitHub Pages repository name
+  base: '/profile', // ✅ Ensure this matches your GitHub Pages repository name
   plugins: [react()],
-  esbuild: {
-    loader: "jsx", // ✅ Ensures JSX is recognized in JS files
-    include: /src\/.*\.js$/, // ✅ Applies only to JS files in /src/
-  },
   css: {
     modules: {
       localsConvention: 'camelCase',
