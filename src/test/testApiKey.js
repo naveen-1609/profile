76baab079ffc1f867b98808fea1c0e2484895cb7
Naveen Venkat <naveenvenkat58@gmail.com>
2025-01-31 22:18:36 -0500

    .env commit

--- src/test/testApiKey.js
+++
@@ -1,4 +0,0 @@
-fetch("https://backend-pfcn.onrender.com/api/key")
-  .then(response => response.json())  // Convert response to JSON
-  .then(data => console.log("API Key Fetched:", data.apiKey)) // Log the API key
-  .catch(error => console.error("Error fetching API key:", error));
